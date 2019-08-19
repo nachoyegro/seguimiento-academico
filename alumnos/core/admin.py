1fb8cb2eb0712e51d84a8d1e7041c4517d47f924
Juan Ignacio Yegro <juan.yegro@unq.edu.ar>
2019-08-19 09:46:47 -0300

    Alumno por carrera

--- alumnos/core/admin.py
+++ alumnos/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.models import Alumno, Materia, MateriaCursada, PlanDeEstudio
+from core.models import Alumno, Materia, MateriaCursada, PlanDeEstudio, Comision
 
 def apellido(obj):
     return obj.datos_personales.apellido
@@ -11,20 +11,18 @@ nombre.short_description = 'Nombre'
 
 class MateriaCursadaTabular(admin.TabularInline):
     model = MateriaCursada
-    fields = ('materia', 'nota')
+    fields = ('comision', 'nota')
     classes = ('grp-collapse grp-open',)
+    raw_id_fields = ('comision', )
 
 class AlumnoAdmin(admin.ModelAdmin):
     list_display = (apellido, nombre,'legajo', 'es_regular')
     search_fields = ('datos_personales__apellido', 'datos_personales__nombre', 'legajo')
-    inlines = [MateriaCursadaTabular, ]
 
 class MateriaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'siglas', 'codigo')
-
-class PlanDeEstudioAdmin(admin.ModelAdmin):
-    readonly_fields = ('materias', )
+    list_display = ('nombre', 'siglas')
 
 admin.site.register(Alumno, AlumnoAdmin)
 admin.site.register(Materia, MateriaAdmin)
-admin.site.register(PlanDeEstudio, PlanDeEstudioAdmin)
+admin.site.register(PlanDeEstudio)
+admin.site.register(Comision)
--- alumnos/core/models.py
+++ alumnos/core/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Materia(models.Model):
     siglas = models.CharField(max_length=32, null=True)
     nombre = models.CharField(max_length=128, null=True)
-    codigo = models.CharField(max_length=10)
 
     def __str__(self):
         return u'%s' % (self.nombre)
@@ -15,48 +14,41 @@ class Comision(models.Model):
     def __str__(self):
         return u'%s' % (self.nombre)
 
-class MateriaEnPlan(models.Model):
-    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=64)
-    tipo = models.CharField(choices=(('b' ,'Basica'), ('a' ,'Avanzada'), ('o' ,'Optativa')), max_length=2)
-    creditos = models.IntegerField(default=0)
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=255)
+    codigo = models.CharField(max_length=2)
 
     def __str__(self):
         return u'%s' % (self.nombre)
 
 class PlanDeEstudio(models.Model):
     nombre = models.CharField(max_length=64)
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
     descripcion = models.TextField(null=True)
-    materias = models.ManyToManyField(MateriaEnPlan)
+    anio = models.CharField(max_length=4)
 
     def __str__(self):
         return u'%s' % (self.nombre)
 
-class Carrera(models.Model):
-    nombre = models.CharField(max_length=255)
-    planes = models.ManyToManyField(PlanDeEstudio)
-    codigo = models.CharField(max_length=2)
+class MateriaEnPlan(models.Model):
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    plan = models.ForeignKey(PlanDeEstudio, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=64)
+    tipo = models.CharField(choices=(('b' ,'Basica'), ('a' ,'Avanzada'), ('o' ,'Optativa')), max_length=2)
+    creditos = models.IntegerField(default=0)
+    codigo = models.CharField(max_length=10)
 
     def __str__(self):
         return u'%s' % (self.nombre)
 
-class Persona(models.Model):
+class Alumno(models.Model):
     nombre = models.CharField(max_length=255)
     apellido = models.CharField(max_length=255)
     dni = models.CharField(max_length=15)
     email = models.CharField(max_length=128)
-
-    def __str__(self):
-        return u'%s, %s' % (self.apellido, self.nombre)
-
-class Alumno(models.Model):
-    datos_personales = models.ForeignKey(Persona, on_delete=models.CASCADE)
-    plan = models.ForeignKey(PlanDeEstudio, null=True, on_delete=models.SET_NULL)
     legajo = models.CharField(max_length=32)
-    carreras = models.ManyToManyField(Carrera)
-    es_regular = models.BooleanField(default=True)
+    es_regular = models.BooleanField(default=True) #TODO: falta averiguar esto
     sexo = models.CharField(choices=(('F', 'Femenino'), ('M', 'Masculino')), max_length=2)
-    cuatrimestre_inscripto = models.CharField(max_length=8, null=True) #Cuatrimestre Ingreso
     telefono = models.CharField(max_length=32, null=True)
     celular = models.CharField(max_length=32, null=True)
     tiene_beca = models.BooleanField(default=False)
@@ -66,44 +58,35 @@ class Alumno(models.Model):
     tiene_portatil = models.BooleanField(default=False)
     comentario = models.CharField(max_length=255, null=True)
     observacion = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return u'%s' % (self.datos_personales)
+
+
+class AlumnoDeCarrera(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    plan = models.ForeignKey(PlanDeEstudio, on_delete=models.CASCADE)
     promedio = models.CharField(max_length=3, null=True)
+    cuatrimestre_inscripto = models.CharField(max_length=8, null=True) #Cuatrimestre Ingreso
 
     def __str__(self):
-        return u'%s' % (self.datos_personales)
-
-class Profesor(models.Model):
-    datos_personales = models.ForeignKey(Persona, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return u'%s' % (self.datos_personales)
-
-class ProfesorEnComision(models.Model):
-    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
-    comision = models.ForeignKey(Comision, on_delete=models.CASCADE)
-    cargo = models.CharField(max_length=64)
-
-    def __str__(self):
-        return u'%s-%s' % (self.profesor, self.comision)
+        return u'%s-%s' % (self.alumno, self.carrera)
 
 class MateriaCursada(models.Model):
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE, related_name='cursadas')
     materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
-    nota = models.CharField(max_length=3, choices=(
+    comision = models.ForeignKey(Comision, null=True, on_delete=models.SET_NULL)
+    nota = models.CharField(max_length=3, default='C', choices=(
+                                ('C', 'Cursando'),
                                 ('EQ', 'Equivalencia'),
                                 ('AP', 'Aprobado'),
                                 ('D', 'Desaprobado'),('1', '1'),('2', '2'),
                                 ('3', '3'),('4', '4'),('5', '5'),('6', '6'),
                                 ('7', '7'), ('8', '8'),('9', '9'),('10', '10')))
+    anio = models.CharField(max_length=4)
+    cuatrimestre = models.CharField(max_length=1, choices=(('1', 'C1'), ('2', 'C2')))
 
     def __str__(self):
-        return u'%s, %s' % (self.materia, self.alumno)
+        return u'%s, %s' % (self.comision.materia, self.alumno)
 
-class Inscripcion(models.Model):
-    """
-        TODO: Tendria que ver como guardar un historial
-    """
-    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
-    comision = models.ForeignKey(Comision, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return u'%s, %s' % (self.comision, self.alumno)
