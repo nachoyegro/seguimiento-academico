5b585768eae53d52006125f5ee75473f3d98c47e
Juan Ignacio Yegro <juan.yegro@unq.edu.ar>
2019-08-29 09:40:57 -0300

    Custom token serializer

---
+++ alumnos/core/token_serializer.py
@@ -0,0 +1,13 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class AlumnosTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Agrego las carreras al payload
+        token['carreras'] = [carrera.pk for carrera in user.profile.carreras.all()]
+        return token
+
+class AlumnosTokenObtainPairView(TokenObtainPairView):
+    serializer_class = AlumnosTokenObtainPairSerializer
--- alumnos/core/urls.py
+++ alumnos/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import *
 from rest_framework import routers, serializers, viewsets, generics
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .token_serializer import AlumnosTokenObtainPairView
 
 router = routers.DefaultRouter()
 router.register(r'materias', MateriasView)
@@ -13,6 +14,6 @@ urlpatterns = [
     path(r'api/v2/', include(router.urls)),
     path(r'api/v1/alumnos/', AlumnosAPIV1.as_view()),
     path(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path(r'api/token/', TokenObtainPairView.as_view()),
+    path(r'api/token/', AlumnosTokenObtainPairView.as_view()),
     path(r'api/token/refresh/', TokenRefreshView.as_view()),
 ]
