448ff5932d5429a0c3a4cbec4dc065e9f0501790
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-02 16:52:41 -0300

    Saco migraciones de sqlite

--- alumnos/importar.sh
+++ alumnos/importar.sh
@@ -4,6 +4,12 @@
 echo 'Borrando BD'
 rm db.sqlite3
 
+echo 'Borrando migraciones viejas'
+rm -rf core/migrations/
+
+echo 'Creando migraciones'
+./manage.py makemigrations core
+
 echo 'Corriendo las migraciones'
 ./manage.py migrate
 
