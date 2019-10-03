e33afae675f1d78622d10c4d6cac55d9023cd29e
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-03 14:47:16 -0300

    Saco create en AlumnoSerializer

--- alumnos/core/serializers.py
+++ alumnos/core/serializers.py
@@ -29,17 +29,7 @@ class MateriaCursadaSerializer(serializers.ModelSerializer):
 
 class AlumnoSerializer(serializers.HyperlinkedModelSerializer):
     cursadas = MateriaCursadaSerializer(many=True, required=False)
-
-    def create(self, validated_data):
-        carreras_data = validated_data.pop('carreras')
-        carreras_ids = [carrera['id'] for carrera in carreras_data]
-        carreras = Carrera.objects.filter(id__in=carreras_ids)
-        alumno = Alumno.objects.create(**validated_data)
-        for carrera in carreras:
-            alumno.carreras.add(carrera)
-        alumno.save()
-        return alumno
-
+    
     class Meta:
         model = Alumno
         fields = ("id", "nombre", "apellido", "email", "legajo", "es_regular", "cursadas", "es_regular")
