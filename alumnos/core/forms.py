886bdcd7530089b10b517d0c3950ee530ea9ce42
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-27 08:23:15 -0300

    Importador de alumnos y refactor de importadores generales

--- alumnos/core/forms.py
+++ alumnos/core/forms.py
@@ -2,20 +2,18 @@ from django import forms
 from django.conf import settings
 from django.core.management import call_command
 
-class ImportarMateriasCursadasForm(forms.Form):
-    materias_file = forms.FileField()
 
-    def __init__(self, *args, **kwargs):
-        super(ImportarMateriasCursadasForm, self).__init__(*args, **kwargs)
-        self.command = 'importar_csv_guarani'
+class ImportadorForm(forms.Form):
+    archivo = forms.FileField()
+    command = ''
 
     def clean(self):
         if not self.errors:
-            materias = self.cleaned_data['materias_file']
-            #Guardo el file
+            materias = self.cleaned_data['archivo']
+            # Guardo el file
             path = settings.MEDIA_URL + materias.name
             self.save_file_in_media(materias, path)
-            call_command('importar_csv_guarani', path)
+            call_command(self.command, path)
             #messages.info(request,'Your export request is being processed')
         return self.cleaned_data
 
@@ -23,3 +21,11 @@ class ImportarMateriasCursadasForm(forms.Form):
         with open(path, 'wb+') as destination:
             for chunk in csv.chunks():
                 destination.write(chunk)
+
+
+class ImportarMateriasCursadasForm(ImportadorForm):
+    command = 'importar_csv_guarani'
+
+
+class ImportarDatosAlumnosForm(ImportadorForm):
+    command = 'importar_alumnos'
--- alumnos/tests/test_form.py
+++ alumnos/tests/test_form.py
@@ -4,29 +4,30 @@ from core.forms import ImportarMateriasCursadasForm
 from core.models import Carrera, Alumno
 from django.conf import settings
 
+
 class ImportarMateriasCursadasFormTest(TestCase):
 
     def setUp(self):
-        #Creo la carrera de IACI
+        # Creo la carrera de IACI
         Carrera.objects.create(codigo='D')
         self.path = '/code/alumnos/docs/alumnos_guarani.csv'
         with open(self.path, 'rb') as infile:
             self.data = {
-                        'materias_file' : SimpleUploadedFile(self.path, infile.read())
-                        }
+                'archivo': SimpleUploadedFile(self.path, infile.read())
+            }
 
     def test_sin_archivo(self):
         form = ImportarMateriasCursadasForm()
         self.assertTrue(not form.is_valid())
 
     def test_con_archivo(self):
-        #El primer argumento es request.POST, el segundo es request.FILES
+        # El primer argumento es request.POST, el segundo es request.FILES
         form = ImportarMateriasCursadasForm({}, self.data)
         self.assertTrue(form.is_valid())
 
     def test_alumnos_creados(self):
         form = ImportarMateriasCursadasForm({}, self.data)
-        #Corro el clean del form para que importe el archivo
+        # Corro el clean del form para que importe el archivo
         form.is_valid()
-        #El csv tiene 3 alumnos
+        # El csv tiene 3 alumnos
         self.assertEquals(Alumno.objects.all().count(), 3)
