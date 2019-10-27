35ff827a93294c4a1ce2dacee570cdd470991bdc
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-27 08:39:47 -0300

    Api v1 no existe mas

--- alumnos/core/views.py
+++ alumnos/core/views.py
@@ -125,35 +125,6 @@ class CarrerasView(viewsets.ModelViewSet):
         return carreras
 
 
-class AlumnosAPIV1(View):
-    def get(self, *args, **kwargs):
-        json = []
-        for alumno in Alumno.objects.all():
-            json_alumno = {"datos_personales": {},
-                           "cursadas": [], "carreras": []}
-            json_alumno["datos_personales"]["nombre"] = alumno.nombre
-            json_alumno["datos_personales"]["apellido"] = alumno.apellido
-            json_alumno["datos_personales"]["dni"] = alumno.dni
-            json_alumno["datos_personales"]["email"] = alumno.email
-            json_alumno["legajo"] = alumno.legajo
-            json_alumno["es_regular"] = alumno.es_regular
-            for cursada in MateriaCursada.objects.filter(alumno=alumno):
-                actual = {"materia": {}}
-                actual["nota"] = cursada.nota
-                actual["materia"]["nombre"] = cursada.materia.nombre
-                actual["materia"]["siglas"] = cursada.materia.siglas
-                actual["materia"]["codigo"] = MateriaEnPlan.objects.filter(
-                    materia=cursada.materia)[0].codigo
-                json_alumno["cursadas"].append(actual)
-            for carrera in AlumnoDeCarrera.objects.filter(alumno=alumno):
-                carrera_json = {}
-                carrera_json["nombre"] = carrera.carrera.nombre
-                carrera_json["codigo"] = carrera.carrera.codigo
-                json_alumno["carreras"].append(carrera_json)
-            json.append(json_alumno)
-        return JsonResponse(json, safe=False)
-
-
 class ImportadorView(View):
     form = None
     template = ''
