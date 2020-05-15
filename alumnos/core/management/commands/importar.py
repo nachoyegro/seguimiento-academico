fb692ec65928fd3d9ac37b934fe79ce77642e39f
Juan Ignacio Yegro <nachoyegro@gmail.com>
2020-05-15 14:04:31 -0300

    Command importar

--- alumnos/core/management/commands/importar.py
+++ alumnos/core/management/commands/importar.py
@@ -9,11 +9,17 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         print('Importando Carreras')
         call_command('importar_carreras')
-        print('Importando planes LIDS')
-        call_command('importar_planes', os.path.join(
-            settings.DOCS_URL, 'planes_lids.csv'))
         print('Importando Cursadas de IACI')
         call_command('importar_csv_guarani', os.path.join(
             settings.DOCS_URL, 'alumnos_guarani.csv'))
         print('Creando SuperUser')
         call_command('create_superuser')
+        print('Importando Planes')
+        call_command('importar_planes', os.path.join(
+            settings.BASE_DIR, 'data/planes_lds.csv'))
+        print('Importando Requisitos')
+        call_command('importar_requisitos', os.path.join(
+            settings.BASE_DIR, 'data/requisitos_lds.csv'))
+        print('Importando Materias Cursadas')
+        call_command('importar_csv_guarani', os.path.join(
+            settings.BASE_DIR, 'data/materias_cursadas_lds.csv'))
