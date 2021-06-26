e56099fac6df1b722e73465c839a802b2e91bf9e
Juan Ignacio Yegro <nachoyegro@gmail.com>
2021-06-26 12:26:11 -0300

    Json dumps utf-8 para cache

--- alumnos/core/management/commands/actualizar_cache.py
+++ alumnos/core/management/commands/actualizar_cache.py
@@ -3,6 +3,7 @@ from core.models import MateriaCursada
 from core.serializers import MateriaCursadaSerializer
 from pymemcache.client.base import PooledClient
 from django.conf import settings
+import json
 
 cache = PooledClient(settings.MEMCACHED_URL, max_pool_size=4, encoding="utf-8")
 
@@ -12,7 +13,7 @@ class Command(BaseCommand):
         carrera = kwargs['carrera']
         materias_cursadas = MateriaCursada.objects.filter(carrera__codigo=carrera)
         data = MateriaCursadaSerializer(materias_cursadas, many=True).data
-        cache.set(carrera, data)
+        cache.set(carrera, json.dumps(data, ensure_ascii=False).encode('utf8'))
         print('La caché fue actualizada')
 
 
